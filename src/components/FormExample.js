6bf659c8969363251ae82e5c68b8503430d6de0d
Lorenz Gerber <gerberlo@gis.a-star.edu.sg>
2018-08-22 16:38:41 +0800

    query front end done

--- src/components/FormExample.js
+++ src/components/FormExample.js
@@ -3,66 +3,120 @@ import "./FormExample.css"
 import { FormGroup , ControlLabel , FormControl , Button } from 'react-bootstrap'
 
 class FormExample extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
 
-    this.state = {
-      value: ''
-    };
-  }
+        this.state = {
+            value: '',
+            chromosome: '',
+            position: '',
+            allele: ''
+        }
+    }
 
-  getValidationState() {
-	  const value = this.state.value
-		if (value === 'X' || value === 'Y' || value === 'MT' ) return 'success'
-		else if ( value > 0 && value < 23 && (value % 1 === 0 ) ) return 'success'
-	 	else if ( value === '') return null
-		else return 'error'
-  }
+    getChromosomeValidationState() {
+        const validation = this.state.chromosome
+        if (validation === 'X' || validation === 'Y' || validation === 'MT' ) return 'success'
+        else if ( validation > 0 && validation < 23 && (validation % 1 === 0 ) ) return 'success'
+        else if ( validation === '') return null
+        else return 'error'
+    }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
+    getPositionValidationState() {
+        const validation = this.state.position
+        if ( validation === '' ) return null
+        else if (!isNaN(validation) && validation % 1 === 0 ) return 'success'
+        else return 'error'
+    }
 
-  render() {
-    return (
-      <form>
-        <FormGroup       
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-    
-        <ControlLabel>Chromosome </ControlLabel>
-        <FormControl
-          type="text"
-          value={this.state.value}
-          placeholder="1-22, X, Y, MT"
-          onChange={this.handleChange}
-        />
-        <FormControl.Feedback />
-        </FormGroup>
-        
-        <FormGroup controlId="positionNumeric">
-        <ControlLabel>Position </ControlLabel>
-        <FormControl
-          type="text"
-          placeholder="numeric position"
-        />
-        </FormGroup>
-      
-        <FormGroup controlId="allele">
-        <ControlLabel>Allele </ControlLabel>
-        <FormControl
-          type="text"
-          placeholder="eg. A, C, G, T"
-        />
-        </FormGroup>
+    getAlleleValidationState() {
+        var bases = RegExp('^[ACGT]*$')
+        const validation = this.state.allele
+        if ( validation === '' ) return null
+        else if ( bases.test(validation) ) return 'success'
+        else return 'error'
+    }
 
-      <Button type="submit">Submit</Button>
-      </form>
-    );
-  }
+    handleChange(event) {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        if ( this.getChromosomeValidationState() === 'success' && 
+             this.getPositionValidationState() === 'success' &&
+             this.getAlleleValidationState() === 'success'
+        ){
+            alert('validated, let\'s query the shit out of it')
+        } else alert( 'you obviously fucked up')
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <form
+            onSubmit={this.handleSubmit}>
+            <FormGroup       
+            controlId="formBasicText"
+            validationState={this.getChromosomeValidationState()}
+            >
+
+            <ControlLabel>Chromosome </ControlLabel>
+            <FormControl
+            name="chromosome"
+            type="text"
+            value={this.state.chromosome}
+            placeholder="1-22, X, Y, MT"
+            onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+            </FormGroup>
+
+            <FormGroup 
+            controlId="formPositionNumeric"
+            validationState={this.getPositionValidationState()}
+            >
+            <ControlLabel>Position </ControlLabel>
+            <FormControl
+            name="position"
+            type="text"
+            value={this.state.position}
+            placeholder="numeric position"
+            onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+            </FormGroup>
+
+            <FormGroup 
+            controlId="allele"
+            validationState={this.getAlleleValidationState()}
+            >
+            <ControlLabel>Allele </ControlLabel>
+            <FormControl
+            name="allele"
+            type="text"
+            value={this.state.allele}
+            placeholder="eg. A, C, G, T"
+            onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+            </FormGroup>
+            <Button type="submit"
+            >Submit</Button>
+            </form>
+            <Alert>
+            Currently no result available
+            </Alert>
+        );
+    }
 }
 
 export default FormExample
